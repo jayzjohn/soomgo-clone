d2586a2fb028036bb7066b125d5ec37635b1e2bf
won-jae <john@zerontech.com>
2023-02-19 18:01:55 +0900

    solve class name bug

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -1,49 +1,52 @@
-import React from 'react';
-import styled, { StyledComponent } from 'styled-components';
+import React from "react";
+import styled, { StyledComponent } from "styled-components";
 
 interface Props {
-    type: 'button' | 'submit' | 'reset';
+    type: "button" | "submit" | "reset";
     variant: string;
     className: string;
     id?: string;
     onClick?: () => void;
     children: string;
-    size: string;
+    size?: string;
 }
+
+const ButtonComponent = styled.button<Props>`
+    position: relative;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    text-decoration: none;
+    vertical-align: middle;
+    cursor: pointer;
+    user-select: none;
+    color: #fff;
+    border-radius: 20px;
+    width: 25rem;
+    padding: 0
+        ${(props) =>
+            props.size === "sm"
+                ? "1.1rem"
+                : props.size === "md"
+                ? "1.4rem"
+                : props.size === "lg"
+                ? "1.6rem"
+                : "1.1rem"};
+    height: ${(props) =>
+        props.size === "sm" ? "34px" : props.size === "md" ? "37px" : props.size === "lg" ? "40px" : "6rem"};
+    font-family: "inter", sans-serif;
+    font-weight: 500;
+    font-size: 3rem;
+    border: 1px solid transparent;
+    background-color: ${(props) => (props.variant === "light" ? "#f8f9fa" : "#01e7ca")};
+    &:hover {
+        background-color: #18fade;
+    }
+`;
 
 const Button = (props: Props) => {
     const { type, variant, className, id, onClick, children, size } = props;
-
-    const ButtonComponent = styled.button<Props>`
-        position: relative;
-        display: inline-flex;
-        align-items: center;
-        justify-content: center;
-        text-align: center;
-        text-decoration: none;
-        vertical-align: middle;
-        cursor: pointer;
-        user-select: none;
-        border-radius: 0.3rem;
-        padding: 0
-            ${(props) =>
-                props.size === 'sm'
-                    ? '1.1rem'
-                    : props.size === 'md'
-                    ? '1.4rem'
-                    : props.size === 'lg'
-                    ? '1.6rem'
-                    : '1.1rem'};
-        height: ${(props) =>
-            props.size === 'sm' ? '34px' : props.size === 'md' ? '37px' : props.size === 'lg' ? '40px' : '34px'};
-        font-family: 'inter', sans-serif;
-        font-weight: 500;
-        border: 1px solid transparent;
-        background-color: ${(props) => (props.variant === 'light' ? '#f8f9fa' : '#1AE5CC')};
-        &:hover {
-            background-color: #18fade;
-        }
-    `;
 
     return (
         <ButtonComponent type={type} className={className} id={id} variant={variant} onClick={onClick} size={size}>
--- layout/body/body.tsx
+++ layout/body/body.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import styles from '@/styles/Home.module.css';
-import Button from '@/components/button/button';
+import React from "react";
+import styles from "@/styles/Home.module.css";
+import Button from "@/components/button/button";
 
 const Body = (): JSX.Element => {
     return (
         <div className={styles.body}>
             <div className={styles.section}>
-                <Button variant='success' type='button' size='sm' className='button-component'>
+                <Button variant='success' type='button' className='button-styled'>
                     button
                 </Button>
-                carusel
             </div>
             <div className={styles.section}></div>
         </div>
