4bccd4ea5b11bdf41b5577e638b2c5b0e1c6d550
won-jae <john@zerontech.com>
2023-03-06 17:11:20 +0900

    get_request_boy

--- server.js
+++ server.js
@@ -2,16 +2,24 @@ const express = require('express')
 const app = express()
 const port = 8080
 const { newEnforcer } = require('casbin');
+const cors = require('cors');
+
 // For browser:
 // import { newEnforcer } from 'casbin';
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+app.use(cors());
+app.use(express.json())
 
 const sub = 'manager'; // the user that wants to access a resource.
 const obj = 'data1'; // the resource that is going to be accessed.
 const act = 'read'; // the operation that the user performs on the resource.
 
 
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200,
+  };
 
 const generateToken = (user = '') => {
     try {
@@ -34,14 +42,15 @@ const generateToken = (user = '') => {
     }
   }
 
-app.get('/login', async (req, res) => {
+app.post('/login',cors(corsOptions), async (req, res) => {
+
+    const {userName, password} = req.body
     const enforcer = await newEnforcer('basic_model.conf', 'basic_policy.csv');
-    const opt = await enforcer.enforce(sub, obj, act);
+    const opt = await enforcer.enforce(userName, obj, act);
+
     if(opt){
         const verify = generateToken(sub)
-        console.log(verify,opt)
-        res.send('Hello World!')
-    
+        res.status(200).json(verify)
     }else{
         res.status(401).send('permission deny')
     }
